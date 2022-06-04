869ea7608787637bd8166e806e839b04cba5633a
helywin <jiang770882022@hotmail.com>
2022-06-04 23:59:36 +0800

    adjust params, add big mario, add coin block

--- player.gd
+++ player.gd
@@ -1,39 +1,66 @@
 extends KinematicBody2D
 
-onready var mario = $mario
-onready var collison = $shape
-export (float) var ACC_NORMAL = 3      # 非加速状态加速度
-export (float) var ACC_AIR = 1.5      # 空中非加速状态加速度
-export (float) var ACC_BOOST = 9     # 加速状态加速度
-export (float) var ACC_SWIM = 0.05     # 游泳加速度
-export (float) var MAX_X_SPEED_NORMAL = 180  # 陆地最大速度
-export (float) var MAX_X_SPEED_BOOST = 8   # 加速状态陆地最大速度
-export (float) var MAX_Y_SPEED_DYNAMIC = 16  # 陆地最大速度
-export (float) var MAX_X_SPEED_SWIM_NORMAL = 1 # 游泳最大速度
-export (float) var MAX_X_SPEED_SWIM_BOOST = 3  # 加速状态游泳最大速度
-export (float) var MAX_Y_SPEED_SWIM_DYNAMIC = 6  # 陆地最大速度
-export (float) var STOP_RATIO = 1.05            # 陆地每帧减速比值
-export (float) var SWIM_STOP_RATIO = 1.03    # 游泳每帧减速比值
-export (float) var JUMP_Y_SPEED = 180 # 陆地起跳初始速度
-export (float) var JUMP_Y_SPEED_ON_WATER = 6 # 水面起跳初始速度
-export (float) var JUMP_Y_SPEED_WATER = 4 # 水里起跳初始速度
-export (float) var GRAVITY = 20 # 引力
-export (float) var GRAVITY_SWIM = 0.2 # 游泳引力
-export (float) var JUMP_ACC = 0.75 # 按住跳跃时持续提供的加速度
-export (float) var JUMP_X_RATIO = 0.2 # 助跑系数
-export (float) var JUMP_X_RATIO_SWIM = 0.1 # 游泳助跑系数
+var mario
+var collison
+
+export (float) var PHYSIC_ACC_NORMAL = 3      # 非加速状态加速度
+export (float) var PHYSIC_ACC_AIR = 1.5      # 空中非加速状态加速度
+export (float) var PHYSIC_ACC_BOOST = 9     # 加速状态加速度
+export (float) var PHYSIC_ACC_SWIM = 0.05     # 游泳加速度
+export (float) var PHYSIC_MAX_X_SPEED_NORMAL = 180  # 陆地最大速度
+export (float) var PHYSIC_MAX_X_SPEED_BOOST = 8   # 加速状态陆地最大速度
+export (float) var PHYSIC_MAX_Y_SPEED_NORMAL = 960  # 陆地最大速度
+export (float) var PHYSIC_MAX_X_SPEED_SWIM_NORMAL = 1 # 游泳最大速度
+export (float) var PHYSIC_MAX_X_SPEED_SWIM_BOOST = 3  # 加速状态游泳最大速度
+export (float) var PHYSIC_MAX_Y_SPEED_SWIM_DYNAMIC = 6  # 陆地最大速度
+export (float) var PHYSIC_STOP_RATIO = 1.05            # 陆地每帧减速比值
+export (float) var PHYSIC_SWIM_STOP_RATIO = 1.03    # 游泳每帧减速比值
+export (float) var PHYSIC_JUMP_Y_SPEED = 180 # 陆地起跳初始速度
+export (float) var PHYSIC_JUMP_Y_SPEED_ON_WATER = 6 # 水面起跳初始速度
+export (float) var PHYSIC_JUMP_Y_SPEED_WATER = 4 # 水里起跳初始速度
+export (float) var PHYSIC_GRAVITY = 20 # 引力
+export (float) var PHYSIC_GRAVITY_SWIM = 0.2 # 游泳引力
+export (float) var PHYSIC_JUMP_ACC = 0.75 # 按住跳跃时持续提供的加速度
+export (float) var PHYSIC_JUMP_X_RATIO = 0.2 # 助跑系数
+export (float) var PHYSIC_JUMP_X_RATIO_SWIM = 0.1 # 游泳助跑系数
+
+export(int, "small", "big", "fire") var CHARACTOR_TYPE = 0
+
+func _get_property_list():
+	var properties = []
+	properties.append({
+			name = "Physic",
+			type = TYPE_NIL,
+			hint_string = "PHYSIC_",
+			usage = PROPERTY_USAGE_GROUP | PROPERTY_USAGE_SCRIPT_VARIABLE
+	})
+	return properties
 
 signal position_changed(position)
 # 公式
 # 纵向速度 = 动态速度 + 0.2 * 关卡引力值
 # 起跳瞬间动态速度 = -(基础值+陆上绿果增益+横向速度增益系数*瞬间横向速度）
 
+# 是否是跳跃中
 var jumping = false
 # 人物的速度
 var velocity = Vector2(0,0)
 
+func is_dead():
+	return false
 
 func _ready():
+	match CHARACTOR_TYPE:
+		0: 
+			mario = $mario_small
+			collison = $collision_small
+			$mario_big.visible = false
+			$collision_big.disabled = true
+		1:
+			mario = $mario_big
+			collison = $collision_big
+			$mario_small.visible = false
+			$collision_small.disabled = true
 	pass # Replace with function body.
 
 var flopping = false
@@ -49,31 +76,40 @@ func _physics_process(delta):
 
 	if jump and is_on_floor():
 		jumping = true
-		velocity.y = -(JUMP_Y_SPEED + (JUMP_X_RATIO * abs(velocity.x)))
+		velocity.y = -(PHYSIC_JUMP_Y_SPEED + (PHYSIC_JUMP_X_RATIO * abs(velocity.x)))
 	if left:
 		if is_on_floor():
-			mario.scale = Vector2(-1, 1)
-			velocity.x -= ACC_NORMAL
+			if mario.scale.x != -1:
+				mario.scale.x = -1
+			else:
+				velocity.x -= PHYSIC_ACC_NORMAL
 		else:
-			velocity.x -= ACC_AIR
+			velocity.x -= PHYSIC_ACC_AIR
 
 
 	if right:
 		if is_on_floor():
-			mario.scale = Vector2(1, 1)
-			velocity.x += ACC_NORMAL
+			if mario.scale.x != 1:
+				mario.scale.x = 1
+			else:
+				velocity.x += PHYSIC_ACC_NORMAL
 		else:
-			velocity.x += ACC_AIR
+			velocity.x += PHYSIC_ACC_AIR
 	
-	if is_on_floor() and abs(velocity.x) < ACC_NORMAL:
+	if is_on_floor() and abs(velocity.x) < PHYSIC_ACC_NORMAL:
 		velocity.x = 0
-	elif not left and not right:
-		velocity.x /= STOP_RATIO
+	elif is_on_floor() and ((velocity.x > 0 and left) or (velocity.x < 0 and right) or not(left or right)):
+		if velocity.x >= 30:
+			velocity.x /= PHYSIC_STOP_RATIO
+		elif abs(velocity.x) < PHYSIC_ACC_NORMAL:
+			velocity.x = 0
+		else:
+			velocity.x += - sign(velocity.x) * PHYSIC_ACC_NORMAL
 
 	if jumping and jump and velocity.y < 0:
-		velocity.y += GRAVITY * 0.25
+		velocity.y += PHYSIC_GRAVITY * 0.25
 	else:
-		velocity.y += GRAVITY
+		velocity.y += PHYSIC_GRAVITY
 	
 	if jumping and not velocity.y < 0 and is_on_floor():
 		mario.animation = "stand"
@@ -83,7 +119,8 @@ func _physics_process(delta):
 	if is_on_floor() and (left or right):
 		mario.animation = "walk"
 	if mario.animation == "walk":
-		mario.speed_scale = abs(velocity.x * 2.5 / MAX_X_SPEED_NORMAL)
+		# 走路速度越快，动作越快，但是不能太慢
+		mario.speed_scale = abs(velocity.x * 2.5 / PHYSIC_MAX_X_SPEED_NORMAL)
 		if mario.speed_scale < 1:
 			mario.speed_scale = 1
 	if not is_on_floor() and not jumping:
@@ -94,71 +131,13 @@ func _physics_process(delta):
 	if is_on_floor():
 		if right and velocity.x < 0 or left and velocity.x > 0:
 			mario.animation = "stop"
-	if is_on_floor() and abs(velocity.x) < ACC_NORMAL:
+	if is_on_floor() and abs(velocity.x) < PHYSIC_ACC_NORMAL:
 		mario.animation = "stand"
-	if abs(velocity.x) > MAX_X_SPEED_NORMAL:
-		velocity.x = sign(velocity.x) * MAX_X_SPEED_NORMAL
+	if abs(velocity.x) > PHYSIC_MAX_X_SPEED_NORMAL:
+		velocity.x = sign(velocity.x) * PHYSIC_MAX_X_SPEED_NORMAL
+	if abs(velocity.y) > PHYSIC_MAX_Y_SPEED_NORMAL:
+		velocity.y = sign(velocity.y) * PHYSIC_MAX_Y_SPEED_NORMAL
 	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
-	# # 碰撞检测
-	# for x in range(s.get_contact_count()):
-	#     var ci = s.get_contact_local_normal(x)
-	#     var pos = s.get_contact_local_position(x)
-	#     if (ci == Vector2(0, -1)):
-	#         print(pos)
-	#         on_ground = true
-	#         lv.y = 0
-	#     elif (ci == Vector2(0, 1)):
-	#         lv.y = -lv.y
-	#         on_ground = true
-	#     else:
-	#         on_ground = true
-			
-	#     if (ci.x * lv.x < 0):
-	#         lv.x = 0
-	
-	# # 处理按键事件
-	# var acc_x = 0
-	# var acc_y = 0
-	# if on_ground:
-	#     if jump:
-	#         mario.animation = "jump"
-	#         lv.y = -real_speed(JUMP_Y_SPEED) - JUMP_X_RATIO * abs(lv.x)
-	#         on_ground = false
-	#     elif move_left:
-	#         mario.scale = Vector2(-1, 1)
-	#         acc_x = -real_speed(ACC_NORMAL)
-	#         mario.animation = "walk"
-	#     elif move_right:
-	#         acc_x = real_speed(ACC_NORMAL)
-	#         mario.scale = Vector2(1, 1)
-	#         mario.animation = "walk"
-			
-	# else: # 在空中
-	#     if (jump && lv.y < 0):
-	#         acc_y = real_speed(GRAVITY - JUMP_ACC)
-	#     else:
-	#         acc_y = real_speed(GRAVITY)
-			
-	# # 引入加速度
-	# lv.x += acc_x
-	# if (!move_left && !move_right):
-	#     if (abs(lv.x) < real_speed(0.05)):
-	#         lv.x = 0
-	#     else:
-	#         lv.x /= STOP_RATIO;
-	
-	# lv.y = lv.y +  acc_y + 0.2 * real_speed(GRAVITY)
-	# if (abs(lv.x) > real_speed(MAX_X_SPEED_NORMAL)):
-	#     lv.x = sign(lv.x) * real_speed(MAX_X_SPEED_NORMAL)
-	# if (abs(lv.y) > real_speed(MAX_Y_SPEED_DYNAMIC)):
-	#     lv.y = sign(lv.y) * real_speed(MAX_Y_SPEED_DYNAMIC)
-	# if on_ground:
-	#     if (move_right && lv.x < 0 || move_left && lv.x > 0):
-	#         mario.animation = "stop"
-	# if abs(lv.x) < real_speed(0.1):
-	#     mario.animation = "stand"
-	# s.set_linear_velocity(lv)
 	emit_signal("position_changed", position)
 	pass
 
